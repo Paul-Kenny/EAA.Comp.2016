39e94161f9c028a05ae82b4fb437460254a08831
John Burns <jzburns@gmail.com>
2016-10-07 10:25:05 +0100

    First push

---
+++ Week1/firstscript.sh
@@ -0,0 +1,19 @@
+#!bin/bash
+
+echo "This is a script to create directories"
+
+#delete first
+rmdir week2/sub1 week2/sub2 week2/sub3
+rmdir week2
+
+#throws eception if exists
+mkdir week2
+
+#create 3 sub dirs
+mkdir week2/sub1 week2/sub2 week2/sub3
+
+
+
+
+
+
