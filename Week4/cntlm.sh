bbf0b77ae35e246cb24a627129547e34e0a67373
John Burns <jzburns@gmail.com>
2016-10-14 10:36:51 +0100

    Week4 changes to pipe.sh

--- Week4/cntlm.sh
+++ Week4/cntlm.sh
@@ -2,4 +2,4 @@
 
 #Append the result of the piped command to a file called "cntlm.txt". The result will contain all the processes on the system that are listed in the "ps -ef" command containing the string "cntlm".  
 
-echo $(ps -ef | grep cntlm) >> cntlm.txt
+echo $(ps -ef | grep gdm-session-worker | grep root) >> cntlm.txt
